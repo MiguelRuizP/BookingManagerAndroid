d9567b94f7d5dd37127f89f98049b872aa44d978
MiguelRuizP <miguelruizpryshlyak.sanjose@alumnado.fundacionloyola.net>
2023-03-12 19:11:34 +0100

    Añadida validación de correo en registro

--- app/src/main/java/com/mruiz/bookingmanagerandroid/activity/RegisterActivity.java
+++ app/src/main/java/com/mruiz/bookingmanagerandroid/activity/RegisterActivity.java
@@ -13,6 +13,8 @@ import com.mruiz.bookingmanagerandroid.api.builder.APIBuilder;
 
 public class RegisterActivity  extends AppCompatActivity {
 
+    String emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,6 +33,8 @@ public class RegisterActivity  extends AppCompatActivity {
 
             if(username.isEmpty() || email.isEmpty() || password.isEmpty()) {
                 Toast.makeText(getApplicationContext(),"You must complete all fields",Toast.LENGTH_SHORT).show();
+            } else if (!email.trim().matches(emailPattern)) {
+                Toast.makeText(getApplicationContext(),"You must enter a valid email",Toast.LENGTH_SHORT).show();
             } else {
                 new Thread(() -> {
                     APIBuilder apiBuilder = new APIBuilder();
@@ -39,6 +43,7 @@ public class RegisterActivity  extends AppCompatActivity {
                     this.runOnUiThread(() -> {
                         if(requestStatus == 200){
                             Toast.makeText(getApplicationContext(),username + " registered successfully!",Toast.LENGTH_SHORT).show();
+                            finish();
                         } else if(requestStatus >= 400 && requestStatus < 500){
                             Toast.makeText(getApplicationContext(), "Couldn't register. Try changing user/email",Toast.LENGTH_LONG).show();
                         } else {
